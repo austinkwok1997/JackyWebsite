bf2095ce642e76c4c4d93be43f5aa3afa69ea1a6
austinkwok1997 <austin.kwok@protonmail.com>
2019-08-02 22:37:59 -0700

    updated photo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import logo from './assets/images/realestate.png'
 
 function App() {
   return (
     <div className="App">
       <h1>Jacky's Fucking Website</h1>
-      <img src="https://i0.kym-cdn.com/photos/images/original/001/262/983/2f0.png" />
+      <img src={logo} />
     </div>
   );
 }
